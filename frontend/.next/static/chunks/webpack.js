c5cd4a8ce8fc0c50320d4e15233c1c349c0b1200
JshineTech <jshinetech@163.com>
2026-02-02 22:47:06 +0800

    auto: claude changes

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7eaea97469831cc3"; }
+/******/ 		__webpack_require__.h = function() { return "22a073aef4874d29"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
